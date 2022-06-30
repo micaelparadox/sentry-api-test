13de8f0e7ddcacbbd81502280c7bf95aae62e36e
Micael Santana <micael.santana@wisereducacao.com>
2022-06-30 19:22:31 -0300

    fix: indentação.

--- src/errorLogs/exceptions/custom-exception.filter.ts
+++ src/errorLogs/exceptions/custom-exception.filter.ts
@@ -3,21 +3,21 @@ import { ErrorLogger } from '../error-logger';
 import { BaseExceptionFilter } from "@nestjs/core";
 
 enum UserResponse {
-    notFound = 404,
-    UnAuthorized = 401,
-  }
+  notFound = 404,
+  UnAuthorized = 401,
+}
 
 @Injectable()
 @Catch(Error, HttpException, BadRequestException)
 export class CustomExceptionFilter extends BaseExceptionFilter {
-    constructor(private errorLogger: ErrorLogger) {
-        super();
-    }
+  constructor(private errorLogger: ErrorLogger) {
+    super();
+  }
 
-    catch(exception: HttpException, host: ArgumentsHost) {
-        if (!(exception instanceof UnauthorizedException || exception instanceof NotFoundException)) {
-            this.errorLogger.sendLog(exception)
-          }
-        super.catch(exception, host);
+  catch(exception: HttpException, host: ArgumentsHost) {
+    if (!(exception instanceof UnauthorizedException || exception instanceof NotFoundException)) {
+      this.errorLogger.sendLog(exception)
     }
+    super.catch(exception, host);
+  }
 }
