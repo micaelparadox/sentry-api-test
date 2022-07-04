353b667d6a04dac65c8ee0f7d8d5a76f7aabd2bc
Micael Santana <micael.santana@wisereducacao.com>
2022-07-04 15:22:35 -0300

    fix: removendo consoleloggeradapter nome

---
+++ src/error-logs/adapters/console-logger.adapter.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@nestjs/common";
+import { LogAdapter } from "../interfaces/log-adapter.interface";
+import { LogData } from "../interfaces/log-data.interface";
+
+
+@Injectable()
+export class ConsoleLoggerAdapter implements LogAdapter {
+    async log(logData: LogData){
+        console.log(logData);
+    }
+}
