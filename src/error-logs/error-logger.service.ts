d641fb128dd7513f84907e1b09f908bd4a96131f
Micael Santana <micael.santana@wisereducacao.com>
2022-07-05 20:04:47 -0300

    fix: error log service logic xchangex

--- src/error-logs/error-logger.service.ts
+++ src/error-logs/error-logger.service.ts
@@ -1,25 +1,28 @@
 import { Injectable } from "@nestjs/common";
+import { SeverityLevels } from "@sentry/types";
 import { ConsoleLoggerAdapter } from "./adapters/console-logger.adapter";
 import { SentryLoggerAdapter } from "./adapters/sentry-logger.adapter";
-import { LogData } from "./interfaces/log-data.interface";
+import { BaseException } from "./base-exception";
+import { SeverityLevel } from "./severity-level";
 
 type AcceptbleLogServices = SentryLoggerAdapter | ConsoleLoggerAdapter
 
 @Injectable()
 export class ErrorLoggerService {
-    constructor(private sentryLoggerAdapter: SentryLoggerAdapter, private consoleLoggerAdapter: ConsoleLoggerAdapter) {
+    constructor(private sentryLoggerAdapter: SentryLoggerAdapter) {
     }
 
-    private LogServices: AcceptbleLogServices[] = [this.sentryLoggerAdapter, this.consoleLoggerAdapter]
+    private LogServices: AcceptbleLogServices[] = [this.sentryLoggerAdapter]
     sendLog(error: Error) {
 
         this.LogServices.forEach((LogService) => {
-            LogService.log(this.convertToLogData(error));
+            LogService.log(this.convertToBaseException(error));
         })
     }
 
-    private convertToLogData(error: Error): LogData {
-        return error
+    private convertToBaseException(error: Error): BaseException {
+        console.log(SeverityLevel.ERROR)
+        return new BaseException(error.message, SeverityLevel.ERROR)
     }
 
 }
